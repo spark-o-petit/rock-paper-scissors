f4098f59848b1464d394d565737b22a82618cbca
Jeong-hwan Choi <jeonghwan@iMac.local>
2020-11-09 00:15:38 +0900

    Completed reset function

--- game.js
+++ game.js
@@ -7,17 +7,42 @@ function playRound (playerSelection, computerSelection) {
     let selectionIndex = 3 + playerSelection - computerSelection;
     switch (selectionIndex % 3) {
         case 0:
-            console.log("Draw!")
+            console.log("Draw!");
             break;
         case 1:
-            console.log("Win!")
+            console.log("Win!");
+            playerScored();
             break;
         case 2:
-            console.log("Lose!")
+            console.log("Lose!");
+            computerScored();
             break;
         default:
             break;
+    };
+
+    if (Number(document.getElementById('playerScore').textContent)==5) {
+        alert("You won the match!!")        
+        resetScore();
+    } else if (Number(document.getElementById('computerScore').textContent)==5) {
+        alert("Computer won the match!!");
+        resetScore();
     }
+}
+
+function playerScored () {
+    let playerWin = Number(document.getElementById('playerScore').textContent) + 1;
+    document.getElementById('playerScore').textContent=playerWin;
+}
+
+function computerScored () {
+    let computerWin = Number(document.getElementById('computerScore').textContent) + 1;
+    document.getElementById('computerScore').textContent=computerWin;
+}
+
+function resetScore () {
+    document.getElementById('playerScore').textContent=0;
+    document.getElementById('computerScore').textContent=0;
 }
 
 const buttons = document.querySelectorAll('button');
